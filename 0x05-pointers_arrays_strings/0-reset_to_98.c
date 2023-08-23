7ceaa867e8f0b677fa750661a11efa78bbb508d4
Mario Otis <otichekube@gmail.com>
2023-08-22 19:19:51 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Tkes a pointer to an interger and point the value to 98
+ *
+ * @n: An interger input pointer
+ *
+ * Return: nothing
+ */
+ void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x005-pointers_arrays_strings
