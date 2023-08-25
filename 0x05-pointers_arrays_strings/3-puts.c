e53bec0ee360330aeb8d37501bc5ebc21c6860d8
Mario Otis <otichekube@gmail.com>
2023-08-24 19:42:13 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,16 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * _puts: This prints a string
+ * 
  * @str: Tjis is the string to be printed
+ *
  * Return: void
  */
 
 void _puts(char *str)
 {
-	for (; *str != '0'; str++)
+	for (; *str != '\0'; str++)
 	{
 	_putchar(*str);
 	}
