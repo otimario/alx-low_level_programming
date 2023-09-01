78a669d482586a5c7936fdf745d91ace8a0095d2
Mario Otis <otichekube@gmail.com>
2023-08-31 23:58:43 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - will calculate the factorial of the gibven number
+ * @in: An input to calculate factorial
+ *
+ * Return: interger
+ *
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
